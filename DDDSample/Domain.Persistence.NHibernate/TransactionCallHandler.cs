0cfb5ebe6b943853dad6be7d9bf253aa06709f0c
SzymonPobiega_cp <7e1df2fc7b7eb1de3ab65aeecb7227e75e7ca2bfAUJZqsgx>
2010-01-06 18:57:41 +0000

    Fixed transaction support.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/dddsamplenet;C40050

--- DDDSample/Domain.Persistence.NHibernate/TransactionCallHandler.cs
+++ DDDSample/Domain.Persistence.NHibernate/TransactionCallHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Transactions;
+using System.Web;
 using Microsoft.Practices.Unity.InterceptionExtension;
 using NHibernate;
+using NHibernate.Context;
 
 namespace DDDSample.Domain.Persistence.NHibernate
 {
@@ -21,15 +25,37 @@ namespace DDDSample.Domain.Persistence.NHibernate
 
       public IMethodReturn Invoke(IMethodInvocation input, GetNextHandlerDelegate getNext)
       {
-         using (ITransaction tx = _sessionFactory.GetCurrentSession().BeginTransaction())
-         {
-            IMethodReturn result = getNext()(input, getNext);
+         IMethodReturn result;
+         SqlConnection sqlConnection = _sessionFactory.GetCurrentSession().Connection as SqlConnection;
+         using (TransactionScope tx = new TransactionScope())
+         {            
+            if (sqlConnection == null)
+            {
+               throw new NotSupportedException("Only SqlConnection is supported.");
+            }
+            sqlConnection.EnlistTransaction(Transaction.Current);
+            result = getNext()(input, getNext);
             if (result.Exception == null)
             {
-               tx.Commit();
-            }
-            return result;
+               _sessionFactory.GetCurrentSession().Flush();
+               tx.Complete();
+            }                        
          }
+         sqlConnection.EnlistTransaction(null);
+         return result;         
+      }
+
+      private ISession BindNHibernateSession()
+      {
+         ISession old = ManagedWebSessionContext.Unbind(HttpContext.Current, _sessionFactory);
+         ManagedWebSessionContext.Bind(HttpContext.Current, _sessionFactory.OpenSession());
+         return old;
+      }
+
+      private void UnbindNHibernateSession(ISession sessionToRestore)
+      {
+         ManagedWebSessionContext.Unbind(HttpContext.Current, _sessionFactory).Dispose();
+         ManagedWebSessionContext.Bind(HttpContext.Current, sessionToRestore);
       }
 
       public int Order { get; set;}      
